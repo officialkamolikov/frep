a908ddf74c72e3674afbc2e1d31e136082a1f9ae
Artem Kogut <web.artem.kogut@icloud.com>
2019-12-16 17:05:48 +0300

    Инициализация приложения

---
+++ frep/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace frep.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
